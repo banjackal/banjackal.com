7121e27a8e7825cd06d9b8ea07bae9e1170c1ad3
Nick Raeon <nick.raeon@gmail.com>
2021-08-14 23:02:09 -0400

    made links open in new tabs

--- site/ClientApp/src/app/app.module.ts
+++ site/ClientApp/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { LinksComponent } from './links/links.component';
-import { AboutComponent } from './about/about.component';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { LinksComponent } from "./links/links.component";
+import { AboutComponent } from "./about/about.component";
 
 @NgModule({
   declarations: [
@@ -16,18 +16,18 @@ import { AboutComponent } from './about/about.component';
     NavMenuComponent,
     HomeComponent,
     LinksComponent,
-    AboutComponent
+    AboutComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'about', component: AboutComponent}
-    ])
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "about", component: AboutComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
