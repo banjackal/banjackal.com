08278a9364ba9c45355c0e585b394726f3844ba5
nick raeon <nick.raeon@gmail.com>
2021-11-14 16:37:01 -0500

    fixed links footer

--- react-spa/src/Components/Links.tsx
+++ react-spa/src/Components/Links.tsx
@@ -1,41 +1,21 @@
 import {Component} from 'react';
 import './Links.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faReddit, faSteam} from '@fortawesome/free-brands-svg-icons';
 
 export class Links extends Component {
-    public goToLink(url: string) {
-      window.open(url);
-    }
-
-    render() {
-        return (
-            <div>
-                <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-/>
-<div className="wrapper menu-links">
-  <a
-    href="https://github.com/banjackal"
-    target="_blank"
-    className="fa fa-github" rel="noreferrer"
-  ></a>
-  <a
-    href="https://twitter.com/nraeon"
-    target="_blank"
-    className="fa fa-twitter"
-  ></a>
-  <a
-    href="https://www.reddit.com/user/banjackal"
-    target="_blank"
-    className="fa fa-reddit"
-  ></a>
-  <a
-    href="https://steamcommunity.com/profiles/76561197984656045/"
-    target="_blank"
-    className="fa fa-steam"
-  ></a>
-</div>
-            </div>
-        )
-    }
+  public goToLink(url: string) {
+    window.open(url);
   }
+
+  render() {
+    return (
+      <div className="wrapper menu-links">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+        <FontAwesomeIcon icon={faTwitter} size="2x"/>
+        <FontAwesomeIcon icon={faReddit} size="2x"/>
+        <FontAwesomeIcon icon={faSteam} size="2x"/>
+      </div>
+    )
+  }
+}
